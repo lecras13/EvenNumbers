767093d5b53da1962886e555d4e97b0b92c5dead
Attiq <lecras@tut.by>
2020-09-18 11:51:38 +0300

    Add NumberformatException

--- src/main/java/com/epam/task/variant/fourth/data/FileDataAcquirer.java
+++ src/main/java/com/epam/task/variant/fourth/data/FileDataAcquirer.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileDataAcquirer implements DataAcquirer {
-
     @Override
     public List<Integer> getNumbers() {
         System.out.println("Numbers from file");
@@ -17,15 +16,15 @@ public class FileDataAcquirer implements DataAcquirer {
         try {
             BufferedReader reader = new BufferedReader(new FileReader("E:\\EvenNumbers\\src\\main\\resources\\FileDataAcquirer.txt"));
             String[] str = (reader.readLine()).split(" ");
-            for (String number: str){
+            for (String number : str) {
                 list.add(Integer.parseInt(number));
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
         }
         return list;
     }

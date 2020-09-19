c0fd38fc081a05e72aa152d4aa33dd2e59a5da74
Attiq <lecras@tut.by>
2020-09-19 10:31:09 +0300

    test changes

--- src/test/java/com/epam/task/variant/fourth/logic/EvenControlTest.java
+++ src/test/java/com/epam/task/variant/fourth/logic/EvenControlTest.java
@@ -9,26 +9,27 @@ import java.util.Arrays;
 import java.util.List;
 
 
-
 public class EvenControlTest {
     @Test
     public void EvenControlTestWhenOneOfNumbersIsEven() throws WrongNumberOfElementsException {
         List<Integer> testList = new ArrayList<Integer>(Arrays.asList(2, 3, 5, 7));
         EvenControl evenControl = new EvenControl();
-        Assert.assertEquals(false, evenControl.moreThanTwoEven(testList));
+        boolean result = evenControl.moreThanTwoEven(testList);
+        Assert.assertEquals(false, result);
     }
 
     @Test
     public void EvenControlTestWhenTwoOrMoreOfNumbersIsEven() throws WrongNumberOfElementsException {
         List<Integer> testList = new ArrayList<Integer>(Arrays.asList(2, 4, 5, 7));
         EvenControl evenControl = new EvenControl();
-        Assert.assertEquals(true, evenControl.moreThanTwoEven(testList));
+        boolean result = evenControl.moreThanTwoEven(testList);
+        Assert.assertEquals(false, result);
     }
 
     @Test(expected = WrongNumberOfElementsException.class)
-        public void EvenControlTestWhenNumbersLessForFourth() throws WrongNumberOfElementsException {
+    public void EvenControlTestWhenNumbersLessForFourth() throws WrongNumberOfElementsException {
         List<Integer> testList = new ArrayList<Integer>(Arrays.asList(2, 6, 7));
         EvenControl evenControl = new EvenControl();
-        evenControl.moreThanTwoEven(testList);
+        boolean result = evenControl.moreThanTwoEven(testList);
     }
 }
